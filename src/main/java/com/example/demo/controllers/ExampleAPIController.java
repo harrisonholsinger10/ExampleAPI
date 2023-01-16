cd19d8afb03331a1eba4565fe203a617ebfd4fb8
Harrison Holsinger <harrisonholsinger@chenmed.com>
2023-01-16 16:30:14 -0500

    Person Example

--- src/main/java/com/example/demo/controllers/ExampleAPIController.java
+++ src/main/java/com/example/demo/controllers/ExampleAPIController.java
@@ -1,19 +1,17 @@
 package com.example.demo.controllers;
 
 import com.example.demo.entities.ExampleAPI;
-import com.example.demo.services.ExampleAPIService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ExampleAPIController {
 
-    @Autowired
-    private ExampleAPIService service;
-
-    @GetMapping("/HelloWorld")
-    String hello_world(){
-        return service.Hello_World();
+    @RequestMapping(value = "/person", method = RequestMethod.POST)
+    public ExampleAPI person_example(@RequestBody ExampleAPI example)
+    {
+        return example;
     }
 }
--- src/main/java/com/example/demo/entities/ExampleAPI.java
+++ src/main/java/com/example/demo/entities/ExampleAPI.java
@@ -1,15 +1,67 @@
 package com.example.demo.entities;
 
 public class ExampleAPI {
+    private String fName;
+    private String lName;
+    private int age;
+    private String email;
+    private String school;
+    private String major;
+    private float gpa;
 
-    private String name;
-
-    public String getName() {
-        return name;
+    public String getfName() {
+        return fName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setfName(String fName) {
+        this.fName = fName;
     }
 
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public float getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(float gpa) {
+        this.gpa = gpa;
+    }
 }
